004b4602feff1f634892ce0d4df2d0a6223e5a26
fzowl <zoltan@voyageai.com>
2025-03-26 22:06:15 +0100

    Comment out the models

--- ebr/models/bgem3.py
+++ ebr/models/bgem3.py
@@ -31,58 +31,58 @@ bge_m3 = ModelMeta(
     similarity="cosine",
     reference="https://huggingface.co/BAAI/bge-m3"
 )
-
-bge_m3_unsupervised = ModelMeta(
-    loader=BGEM3EmbeddingModel,
-    model_name='BAAI/bge-m3-unsupervised',
-    embd_dtype="float32",
-    embd_dim=1024,
-    num_params=22_700_000,
-    max_tokens=8192,
-    similarity="cosine",
-    reference="https://huggingface.co/BAAI/bge-m3-unsupervised"
-)
-
-bge_m3_retromae = ModelMeta(
-    loader=BGEM3EmbeddingModel,
-    model_name='BAAI/bge-m3-retromae',
-    embd_dtype="float32",
-    embd_dim=1024,
-    num_params=22_700_000,
-    max_tokens=8192,
-    similarity="cosine",
-    reference="https://huggingface.co/BAAI/bge-m3-retromae"
-)
-
-bge_large_en_v15 = ModelMeta(
-    loader=BGEM3EmbeddingModel,
-    model_name='BAAI/bge-large-en-v1.5',
-    embd_dtype="float32",
-    embd_dim=1024,
-    num_params=22_700_000,
-    max_tokens=512,
-    similarity="cosine",
-    reference="https://huggingface.co/BAAI/bge-large-en-v1.5"
-)
-
-bge_base_en_v15 = ModelMeta(
-    loader=BGEM3EmbeddingModel,
-    model_name='BAAI/bge-base-en-v1.5',
-    embd_dtype="float32",
-    embd_dim=768,
-    num_params=22_700_000,
-    max_tokens=512,
-    similarity="cosine",
-    reference="https://huggingface.co/BAAI/bge-base-en-v1.5"
-)
-
-bge_small_en_v15 = ModelMeta(
-    loader=BGEM3EmbeddingModel,
-    model_name='BAAI/bge-small-en-v1.5',
-    embd_dtype="float32",
-    embd_dim=384,
-    num_params=22_700_000,
-    max_tokens=512,
-    similarity="cosine",
-    reference="https://huggingface.co/BAAI/bge-small-en-v1.5"
-)
+#
+# bge_m3_unsupervised = ModelMeta(
+#     loader=BGEM3EmbeddingModel,
+#     model_name='BAAI/bge-m3-unsupervised',
+#     embd_dtype="float32",
+#     embd_dim=1024,
+#     num_params=22_700_000,
+#     max_tokens=8192,
+#     similarity="cosine",
+#     reference="https://huggingface.co/BAAI/bge-m3-unsupervised"
+# )
+#
+# bge_m3_retromae = ModelMeta(
+#     loader=BGEM3EmbeddingModel,
+#     model_name='BAAI/bge-m3-retromae',
+#     embd_dtype="float32",
+#     embd_dim=1024,
+#     num_params=22_700_000,
+#     max_tokens=8192,
+#     similarity="cosine",
+#     reference="https://huggingface.co/BAAI/bge-m3-retromae"
+# )
+#
+# bge_large_en_v15 = ModelMeta(
+#     loader=BGEM3EmbeddingModel,
+#     model_name='BAAI/bge-large-en-v1.5',
+#     embd_dtype="float32",
+#     embd_dim=1024,
+#     num_params=22_700_000,
+#     max_tokens=512,
+#     similarity="cosine",
+#     reference="https://huggingface.co/BAAI/bge-large-en-v1.5"
+# )
+#
+# bge_base_en_v15 = ModelMeta(
+#     loader=BGEM3EmbeddingModel,
+#     model_name='BAAI/bge-base-en-v1.5',
+#     embd_dtype="float32",
+#     embd_dim=768,
+#     num_params=22_700_000,
+#     max_tokens=512,
+#     similarity="cosine",
+#     reference="https://huggingface.co/BAAI/bge-base-en-v1.5"
+# )
+#
+# bge_small_en_v15 = ModelMeta(
+#     loader=BGEM3EmbeddingModel,
+#     model_name='BAAI/bge-small-en-v1.5',
+#     embd_dtype="float32",
+#     embd_dim=384,
+#     num_params=22_700_000,
+#     max_tokens=512,
+#     similarity="cosine",
+#     reference="https://huggingface.co/BAAI/bge-small-en-v1.5"
+# )
