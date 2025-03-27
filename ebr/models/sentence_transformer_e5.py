3cbd10335158a8945d957449c5146ea94b351235
fzowl <zoltan@voyageai.com>
2025-03-27 23:10:13 +0100

    Put the dataset metadata to results.json

--- ebr/models/sentence_transformer_e5.py
+++ ebr/models/sentence_transformer_e5.py
@@ -13,8 +13,6 @@ class E5EmbeddingModel(SentenceTransformersEmbeddingModel):
 #     model_name="e5-mistral-7b-instruct",
 #     embd_dtype="float32",
 #     embd_dim=4096,
-#     num_params=22_700_000,
-#     max_tokens=256,
 #     similarity="cosine",
 #     reference="https://huggingface.co/intfloat/e5-mistral-7b-instruct"
 # )
