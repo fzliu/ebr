07debf4dc848fb6b025d7a947d44045d780b2512
fzowl <zoltan@voyageai.com>
2025-03-30 13:22:11 +0200

    - correcting the BGEM3 and GritLM model params  - using both Google's vertex ai API and genai API

--- ebr/models/gritlm.py
+++ ebr/models/gritlm.py
@@ -24,22 +24,22 @@ class GRITLMEmbeddingModel(EmbeddingModel):
         return [[float(str(x)) for x in result[i]] for i in range(len(result))]
 
 
-gritlm_7b = ModelMeta(
-    loader=GRITLMEmbeddingModel,
-    model_name="GritLM/GritLM-7B",
-    embd_dtype="float32",
-    embd_dim=384,
-    num_params=7_240_000,
-    similarity="cosine",
-    reference="https://huggingface.co/GritLM/GritLM-7B"
-)
-
-gritlm_8x7b = ModelMeta(
-    loader=GRITLMEmbeddingModel,
-    model_name="GritLM/GritLM-8x7B",
-    embd_dtype="float32",
-    embd_dim=384,
-    num_params=46_700_000,
-    similarity="cosine",
-    reference="https://huggingface.co/GritLM/GritLM-8x7B"
-)
+# gritlm_7b = ModelMeta(
+#     loader=GRITLMEmbeddingModel,
+#     model_name="GritLM/GritLM-7B",
+#     embd_dtype="float32",
+#     embd_dim=4096,
+#     num_params=7_240_000_000,
+#     similarity="cosine",
+#     reference="https://huggingface.co/GritLM/GritLM-7B"
+# )
+#
+# gritlm_8x7b = ModelMeta(
+#     loader=GRITLMEmbeddingModel,
+#     model_name="GritLM/GritLM-8x7B",
+#     embd_dtype="float32",
+#     embd_dim=4096,
+#     num_params=46_700_000_000,
+#     similarity="cosine",
+#     reference="https://huggingface.co/GritLM/GritLM-8x7B"
+# )
